4329b011d0b5a0ae74ff3a8aeb9495cba7358249
Victor Santiago <victorhsantiago3@gmail.com>
2020-07-25 13:46:38 -0300

    refactor: separate data into their own file

--- index.js
+++ index.js
@@ -5,30 +5,7 @@ const {
   sortByDeadline,
 } = require('./utils')
 
-const initialDate = '2019-11-10 09:00:00'
-const finalDate = '2019-11-11 12:00:00'
-const batchHours = 8
-
-const jobs = [
-  {
-    id: 1,
-    description: 'Importação de arquivos de fundos',
-    deadline: '2019-11-10 12:00:00',
-    duration: 2,
-  },
-  {
-    id: 2,
-    description: 'Importação de dados da Base Legada',
-    deadline: '2019-11-11 12:00:00',
-    duration: 4,
-  },
-  {
-    id: 3,
-    description: 'Importação de dados de integração',
-    deadline: '2019-11-11 08:00:00',
-    duration: 6,
-  },
-]
+const { initialDate, finalDate, batchHours, jobs } = require('./data')
 
 console.log(
   getSchedule({
