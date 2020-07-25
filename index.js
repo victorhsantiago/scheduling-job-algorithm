66c1b64d30425183e5b5abb556a070385e3047bd
Victor Santiago <victorhsantiago3@gmail.com>
2020-07-25 10:21:19 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const initalDate = '2019-11-10 09:00:00'
+const finalDate = '2019-11-11 12:00:00'
+
+const jobs = [
+  {
+    id: 1,
+    description: 'Importação de arquivos de fundos',
+    deadline: '2019-11-10 12:00:00',
+    duration: 2,
+  },
+  {
+    id: 2,
+    description: 'Importação de dados da Base Legada',
+    deadline: '2019-11-11 12:00:00',
+    duration: 4,
+  },
+  {
+    id: 3,
+    description: 'Importação de dados de integração',
+    deadline: '2019-11-11 08:00:00',
+    duration: 6,
+  },
+]
